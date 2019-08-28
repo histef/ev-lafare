94f8630c02c332a8f904b1150af0146b7d8398b2
Stefanie Chin <hi.stef.c@gmail.com>
2019-08-27 18:46:14 -0700

    got video working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef, Fragment} from 'react';
 import { Route } from "react-router-dom";
 import './App.css';
+import EVLF_animation_2D from './EVLF_animation_2D.mp4';
 
 import Home from './Home';
 import About from './About';
@@ -9,30 +10,39 @@ import Books from './Books';
 import Navbar from './Navbar';
 
 function App() {
-  const [bgColor, setBGColor] = useState('#ffbd2c')
-
-  useEffect(()=>{
-    console.log(bgColor)
-  })
+  const [bgColor, setBGColor] = useState('#ffbd2c');
+  const [goToHomePage, setGoToHomePage] = useState(false);
+  const vidRef = useRef();
 
   const handleBgColor = (color) => {
     setBGColor(color)
   }
 
+  useEffect(()=> {
+    setTimeout(() => setGoToHomePage(true), 7000)
+  },[])
+
   return ( 
     <div className="App" style={{display: 'flex',  height: '100vh', width: '100vw'}}>
-      <main style={{width: '90%', backgroundColor: `${bgColor}`, paddingLeft: "30px"}}>
-      <Navbar />
-        <Route exact path='/' render={(props) => <Home handleBgColor={handleBgColor}/>} />
-        <Route exact path='/about' render={(props) => <About handleBgColor={handleBgColor}/>} />
-        <Route exact path='/books' render={(props) => <Books handleBgColor={handleBgColor}/>}  />
-        <Route exact path='/contact' render={(props) => <Contact handleBgColor={handleBgColor}/>}  />
-      </main>
-      <section style={{width: '10%', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'flex-end', backgroundColor: `${bgColor}` }}>
-        <i class="fab fa-twitter fa-3x" style={{width: '100%'}}></i>
-        <i class="fab fa-instagram fa-3x" style={{width: '100%'}}></i>
-        <i class="fab fa-twitch fa-3x" style={{width: '100%', paddingBottom: '100px'}}></i>
-      </section>
+    {goToHomePage ?
+      (<Fragment>
+        <main style={{width: '90%', backgroundColor: `${bgColor}`, paddingLeft: "30px"}}>
+        <Navbar />
+          <Route exact path='/' render={(props) => <Home handleBgColor={handleBgColor}/>} />
+          <Route exact path='/about' render={(props) => <About handleBgColor={handleBgColor}/>} />
+          <Route exact path='/books' render={(props) => <Books handleBgColor={handleBgColor}/>}  />
+          <Route exact path='/contact' render={(props) => <Contact handleBgColor={handleBgColor}/>}  />
+        </main>
+        <section style={{width: '10%', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'flex-end', backgroundColor: `${bgColor}` }}>
+          <i className="fab fa-instagram fa-3x" style={{width: '100%'}}></i>
+          <i className="fab fa-twitter fa-3x" style={{width: '100%'}}></i>
+          <i className="fab fa-twitch fa-3x" style={{width: '100%', paddingBottom: '100px'}}></i>
+        </section>
+      </Fragment>)
+      : (<video width="100%" height="100%" muted autoPlay>
+          <source src={EVLF_animation_2D} type="video/mp4"></source>
+        </video>)
+      }
     </div>
   );
 }
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,8 +6,17 @@ function Contact(props) {
   },[])
 
   return (
-    <div className="about" style={{backgroundColor: '#ed985f'}}>
-      <h1 style={{fontSize: '4rem', letterSpacing: ".4rem"}}>CONTACT</h1>
+    <div className="about" style={{backgroundColor: '#ed985f', display: 'flex'}}>
+      <h1 style={{width: '30%', fontSize: '4rem', letterSpacing: ".4rem"}}>CONTACT</h1>
+      <div style={{width: '60%'}}>
+        <h2 style={{fontSize: '2rem', letterSpacing: ".4rem"}}>AGENT</h2>
+        <p><strong>Claire Friedman</strong>
+             Inkwell Management
+             521 Fifth Ave., Ste. 2600
+              New York, NY 10175
+             212.922.3500
+        </p>
+      </div>
     </div>
   );
 }
