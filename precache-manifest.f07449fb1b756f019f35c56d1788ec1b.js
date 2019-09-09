6d2eca5f8eeced88bd8ae14a94c51deea8cda5da
Stefanie Chin <hi.stef.c@gmail.com>
2019-09-08 18:13:44 -0700

    Updates

---
+++ precache-manifest.f07449fb1b756f019f35c56d1788ec1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfb14abd5a73cc832eb5dcec23cdc892",
+    "url": "/ev-lafare/index.html"
+  },
+  {
+    "revision": "adda404a8930b23a0cb9",
+    "url": "/ev-lafare/static/css/main.2931053c.chunk.css"
+  },
+  {
+    "revision": "5d542bee8e9474d027b2",
+    "url": "/ev-lafare/static/js/2.78afe261.chunk.js"
+  },
+  {
+    "revision": "adda404a8930b23a0cb9",
+    "url": "/ev-lafare/static/js/main.fdff7e8e.chunk.js"
+  },
+  {
+    "revision": "7e4fbed42d335b776a70",
+    "url": "/ev-lafare/static/js/runtime~main.3f29c2c7.js"
+  },
+  {
+    "revision": "1762df560ebbc1c8bd4db193266a0f2f",
+    "url": "/ev-lafare/static/media/EVLF_animation_2D.1762df56.mp4"
+  },
+  {
+    "revision": "9ab2aa0adf6f76d6adc478d906d6b65a",
+    "url": "/ev-lafare/static/media/Ev_aboutpic1_500px.9ab2aa0a.jpg"
+  }
+]);
