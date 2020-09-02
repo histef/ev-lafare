ffbd591481083a8c31ba3cbc0b28de0a1f57b386
Stefanie <35500567+histef@users.noreply.github.com>
2020-09-02 15:37:40 -0700

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -13,7 +13,7 @@ function About(props) {
       <h1 style={{width: '30%', minWidth: '300px', marginBottom: '0', paddingLeft: '30px', fontSize: '4rem', letterSpacing: ".4rem"}}>ABOUT</h1>
       <div style={{display: 'flex', flexWrap: 'wrap', position: 'absolute', top: '35%', left: '25%', color: '#3e3e3e'}}>
           <img src={Ev_aboutpic1_500px} style={{zIndex: '1', height: '300px'}}></img>
-          <p style={{width: '215px', paddingLeft: '20px'}}>Ev LaFare spent his early years in the south Midwest scribbling poems, practicing Rachmaninoff preludes, and perfecting combos in <i>Street Fighter II</i>. After relocating to Los Angeles, Ev found success as a songwriter and music producer. Since then, LaFare has switched his focus to writing. Between sessions, shows, (and games of <i>Overwatch</i>), his first YA novel was born.</p>
+          <p style={{width: '215px', paddingLeft: '20px'}}>Ev LaFare spent his early years in the south Midwest scribbling poems, practicing Rachmaninoff preludes, and perfecting combos in <i>Street Fighter II</i>. After relocating to Los Angeles, Ev found success as a songwriter and music producer. Since then, LaFare has switched his focus to writing. In June of 2019, he signed with Claire Friedman of Inkwell Management. He is currently working on his debut novel.</p>
       </div>
     </div>
   );
